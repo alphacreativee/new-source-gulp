8322cf527ba33b9d0f93da4639e4ad928493e7b0
Taoo <hai.alphacreative@gmail.com>
2026-01-06 14:40:38 +0700

    add new global js

---
+++ assets/js/index/global.js
@@ -0,0 +1,76 @@
+export function customDropdown() {
+  const dropdowns = document.querySelectorAll(
+    ".dropdown-custom, .dropdown-custom-select"
+  );
+
+  dropdowns.forEach((dropdown) => {
+    const btnDropdown = dropdown.querySelector(".dropdown-custom-btn");
+    const dropdownMenu = dropdown.querySelector(".dropdown-custom-menu");
+    const dropdownItems = dropdown.querySelectorAll(".dropdown-custom-item");
+    const valueSelect = dropdown.querySelector(".value-select");
+    const displayText = dropdown.querySelector(".dropdown-custom-text");
+
+    const isSelectType = dropdown.classList.contains("dropdown-custom-select");
+
+    btnDropdown.addEventListener("click", function (e) {
+      e.stopPropagation();
+      closeAllDropdowns(dropdown);
+      dropdownMenu.classList.toggle("dropdown--active");
+      btnDropdown.classList.toggle("--active");
+    });
+
+    document.addEventListener("click", function () {
+      closeAllDropdowns();
+    });
+
+    dropdownItems.forEach((item) => {
+      item.addEventListener("click", function (e) {
+        e.stopPropagation();
+
+        if (isSelectType) {
+          const optionText = item.textContent;
+          displayText.textContent = optionText;
+          dropdown.classList.add("selected");
+        } else {
+          const currentImgEl = valueSelect.querySelector("img");
+          const currentImg = currentImgEl ? currentImgEl.src : "";
+          const currentText = valueSelect.querySelector("span").textContent;
+          const clickedHtml = item.innerHTML;
+
+          valueSelect.innerHTML = clickedHtml;
+
+          const isSelectTime = currentText.trim() === "Time";
+
+          if (!isSelectTime) {
+            if (currentImg) {
+              item.innerHTML = `<span>${currentText}</span><img src="${currentImg}" alt="" />`;
+            } else {
+              item.innerHTML = `<span>${currentText}</span>`;
+            }
+          }
+        }
+
+        closeAllDropdowns();
+      });
+    });
+
+    window.addEventListener("scroll", function () {
+      if (dropdownMenu.closest(".header-lang")) {
+        dropdownMenu.classList.remove("dropdown--active");
+        btnDropdown.classList.remove("--active");
+      }
+    });
+  });
+
+  function closeAllDropdowns(exception) {
+    dropdowns.forEach((dropdown) => {
+      const menu = dropdown.querySelector(".dropdown-custom-menu");
+      const btn = dropdown.querySelector(".dropdown-custom-btn");
+
+      if (!exception || dropdown !== exception) {
+        menu.classList.remove("dropdown--active");
+        btn.classList.remove("--active");
+      }
+    });
+  }
+}
--- assets/js/index/script.js
+++ assets/js/index/script.js
@@ -1,4 +1,5 @@
 import { preloadImages } from "../../libs/utils.js";
+import { customDropdown } from "../../js/index/global.js";
 ("use strict");
 $ = jQuery;
 
@@ -12,6 +13,7 @@ gsap.ticker.lagSmoothing(0);
 
 const init = () => {
   gsap.registerPlugin(ScrollTrigger);
+  customDropdown();
 };
 preloadImages("img").then(() => {
   init();
